2b3d477600b76a5c09090efc8c0b25fe9591e4d9
ezequiel@adaitw.com.ar <ezequiel@adaitw.com.ar>
2020-06-26 21:05:39 +0000

    Mumuki Export on 2020-06-26 21:05:39 +0000

--- 00001_Nuevo ejercicio/test..js
+++ 00001_Nuevo ejercicio/test..js
@@ -1,5 +1,5 @@
 describe("Descripción del grupo", function() {
   it("Cada valor tiene +10", function() {
-    [1, 2, 3, 4, 5].map(n => n + 10).to.eql(numerosMasDiez)
+    [1, 2, 3, 4, 5].to.eql(numerosMasDiez)
   })
 })
